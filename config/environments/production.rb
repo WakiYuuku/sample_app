38defc5fb86892e788397ef8117f16cebd92984b
和木優空 <wakiyuku@TEC-022-0122.local>
2024-04-30 17:43:58 +0900

    Add account activation

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -98,7 +98,8 @@ Rails.application.configure do
   ActionMailer::Base.smtp_settings = {
     :port           => 587,
     :address        => 'smtp.mailgun.org',
-    :user_name      => ENV['MAILGUN_SMTP_LOGIN'],
+    #:user_name      => ENV['MAILGUN_SMTP_LOGIN'],
+    user_name:      'apikey',
     :password       => ENV['MAILGUN_SMTP_PASSWORD'],
     :domain         => host,
     :authentication => :plain,
